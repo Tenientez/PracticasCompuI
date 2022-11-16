f96f4900abe94fe5998e4c15ea75128318af719e
Oscar Teniente <111546056+Tenientez@users.noreply.github.com>
2022-11-16 10:18:36 -0600

    Add files via upload

---
+++ Puntos/Punto.h
@@ -0,0 +1,30 @@
+#include <array>
+#include <cmath>
+#include <math.h>
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+
+class Punto2D {
+public:
+    float GetX();
+    float GetY();
+    void SetPosicion(float x, float y);
+    void Trasladar(float temp1, float temp2);
+    void Escalar(float temp1, float temp2);
+    void RotarRespectoAlOrigen(float temp1);
+
+
+
+private:
+
+    array <float, 2> Pi {};
+    array < array <float,2>, 2> Rot {};
+
+};
+
+#endif //PUNTOS_PUNTO_H
